0a97e5576a08b24ca4ae3f8721c0c4ec0c30c403
coder_ronnie <royalronnie05@gmail.com>
2025-05-23 18:22:57 +0530

    Initial commit of updated portfolio

---
+++ src/components/Hero/HeroSection.jsx
@@ -0,0 +1,148 @@
+import "./Hero.css";
+import { useContext, useState, useRef, useEffect } from "react";
+import { ToggleContext } from "../../store/Toggle-Context";
+import bgVideo from "../../assets/bg.mp4";
+import { motion } from "framer-motion";
+import { ChevronDown } from "lucide-react";
+
+export default function HeroSection({ abtref }) {
+  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+  const [isLargeScreen, setIsLargeScreen] = useState(window.innerWidth >= 768);
+  const videoRef = useRef(null);
+
+  useEffect(() => {
+    const handleMouseMove = (e) => {
+      setMousePosition({ x: e.clientX, y: e.clientY });
+    };
+
+    const handleResize = () => {
+      const largeScreen = window.innerWidth >= 768;
+      setIsLargeScreen(largeScreen);
+      if (largeScreen) {
+        window.addEventListener("mousemove", handleMouseMove);
+      } else {
+        window.removeEventListener("mousemove", handleMouseMove);
+      }
+    };
+
+    window.addEventListener("resize", handleResize);
+    handleResize();
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+      window.removeEventListener("mousemove", handleMouseMove);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (videoRef.current) {
+      videoRef.current.playbackRate = 0.75; // Slow down the video playback
+    }
+  }, []);
+
+  function handleScrollAbout() {
+    if (abtref.current) {
+      abtref.current.scrollIntoView({ behavior: "smooth" });
+    }
+  }
+
+  const { theme } = useContext(ToggleContext);
+
+  let classes = "hero-content centerDiv fixed h-full w-full ";
+
+  if (theme === "light") {
+    classes += " bg-gradient-to-b from-gray-700 via-gray-500 to-gray-400 ";
+  } else {
+    classes +=
+      " bg-gradient-to-b from-black via-gray-800 to-gray-800 text-[#D4A03E] ";
+  }
+
+  return (
+    <div
+      className="bigContainer flex justify-center items-center z-0"
+      id="home"
+    >
+      <div className={classes}>
+        <div className="content relative z-10 flex flex-col items-center justify-center h-full px-4">
+          <h3 className="mb-3 font-semibold text-xl">Hello, I&apos;m </h3>
+          <motion.h1
+            initial={{ opacity: 0, y: -50 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.8, delay: 0.2 }}
+            className="font-sans text-4xl sm:text-5xl md:text-6xl font-bold mb-4 text-shadow text-center"
+          >
+            SACHIN SAINE
+          </motion.h1>
+          <motion.h2
+            initial={{ opacity: 0, y: 50 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.8, delay: 0.4 }}
+            className="font-sans text-xl sm:text-2xl md:text-3xl mb-8 text-shadow text-center"
+          >
+            SOFTWARE ENGINEER
+          </motion.h2>
+          <motion.button
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.9 }}
+            onClick={handleScrollAbout}
+            className="bg-white bg-opacity-20 backdrop-blur-sm text-gray-900 p-3 md:p-4 rounded-full shadow-lg hover:bg-opacity-30 transition duration-300 group focus:outline-none focus:ring-4 focus:ring-[#D4A03E] focus:ring-opacity-50"
+            aria-label="Scroll to about section"
+          >
+            <motion.div
+              animate={{ y: [0, 5, 0] }}
+              transition={{
+                repeat: Infinity,
+                duration: 1.5,
+                ease: "easeInOut",
+              }}
+            >
+              <ChevronDown className="w-6 h-6 md:w-8 md:h-8 group-hover:text-[#D4A03E] transition-colors duration-300" />
+            </motion.div>
+          </motion.button>
+        </div>
+
+        {isLargeScreen ? (
+          <video
+            ref={videoRef}
+            autoPlay
+            loop
+            muted
+            playsInline
+            loading="lazy"
+            className={`absolute top-0 left-0 w-full h-full object-cover -z-10 ${
+              theme === "light" ? "light-theme-video" : ""
+            }`}
+          >
+            <source src={bgVideo} type="video/mp4" />
+            Your browser does not support the video tag.
+          </video>
+        ) : (
+          <div
+            className={`absolute inset-0 ${
+              theme === "light"
+                ? "bg-gradient-to-b from-gray-700 via-gray-500 to-gray-400"
+                : "bg-gradient-to-b from-black via-gray-800 to-gray-800"
+            } -z-10`}
+          />
+        )}
+
+        <div className="absolute inset-0 bg-black opacity-60 -z-5" />
+        {isLargeScreen && (
+          <div
+            className="absolute inset-0 opacity-30"
+            style={{
+              backgroundImage: `radial-gradient(circle at ${
+                mousePosition.x
+              }px ${mousePosition.y}px, ${
+                theme === "light"
+                  ? "rgba(107, 114, 128, 1)"
+                  : "rgba(212, 160, 62, 1)"
+              } 0%, transparent 15%)`,
+              zIndex: "inherit",
+            }}
+          />
+        )}
+      </div>
+    </div>
+  );
+}
---
+++ src/components/Socials.jsx
@@ -0,0 +1,98 @@
+import React from "react";
+import { FaLinkedin } from "react-icons/fa";
+import { AiFillInstagram } from "react-icons/ai";
+import { FaGithub } from "react-icons/fa";
+import { SiCodesandbox } from "react-icons/si";
+import { FaSquareUpwork } from "react-icons/fa6";
+import { useState, useEffect } from "react";
+import { ChevronUp } from "lucide-react";
+import { motion } from "framer-motion";
+const Socials = () => {
+  const [showScrollButton, setShowScrollButton] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const contactSection = document.getElementById("contact");
+      const socialsDiv = document.getElementById("socials");
+
+      if (contactSection && socialsDiv) {
+        const contactTop = contactSection.getBoundingClientRect().top;
+        const isVisible = contactTop <= window.innerHeight / 2;
+
+        setShowScrollButton(isVisible);
+        socialsDiv.style.display = isVisible ? "none" : "flex";
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  return (
+    <>
+      <div
+        id="socials"
+        className="socials fixed bottom-4 lg:top-1/2 lg:right-4 transform lg:-translate-y-1/2 -translate-x-1/2 /md:left-auto lg:left-auto left-1/2  z-[90] bg-white/30 text-black backdrop-blur-lg p-4 rounded-lg shadow-lg  lg:space-y-0 lg:space-x-4 transition-all duration-300 opacity-80 hover:opacity-100  flex flex-row lg:flex-col justify-center items-center lg:h-[35%] md:h-[30%] lg:hover:translate-x-0 lg:translate-x-10 "
+      >
+        <div className="flex lg:flex-col  xl:flex-col 2xl:flex-col flex-row items-center">
+          <a
+            href="https://www.linkedin.com/in/sachin-saine-75b641274/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="mb-1 lg:mb-1 mr-1 hover:text-blue-600 cursor-pointer text-3xl"
+          >
+            <FaLinkedin />
+          </a>
+          <a
+            href="https://www.instagram.com/itz_royal1916/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="mb-1 lg:mb-1 mr-1 hover:text-pink-500 cursor-pointer text-3xl"
+          >
+            <AiFillInstagram />
+          </a>
+          <a
+            href="https://github.com/Sachinsaine"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="mb-1 lg:mb-1 mr-1 hover:text-gray-500 cursor-pointer text-3xl"
+          >
+            <FaGithub />
+          </a>
+          <a
+            href="https://codesandbox.io"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="mb-1 lg:mb-1 mr-1 hover:text-purple-500 cursor-pointer text-3xl"
+          >
+            <SiCodesandbox />
+          </a>
+          <a
+            href="https://www.upwork.com"
+            target="_blank"
+            rel="noopener noreferrer"
+            className=" mb-1 lg:mb-1 mr-1 hover:text-green-500 cursor-pointer text-3xl"
+          >
+            <FaSquareUpwork />
+          </a>
+        </div>
+      </div>
+      {showScrollButton && (
+        <motion.button
+          onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
+          className="bg-white bg-opacity-20 backdrop-blur-sm text-gray-900 p-3 md:p-4 rounded-full shadow-lg hover:bg-opacity-30 transition duration-300 group focus:outline-none focus:ring-4 focus:ring-[#D4A03E] focus:ring-opacity-50 fixed bottom-4 left-1/2 transform -translate-x-1/2 z-50"
+          aria-label="Scroll to about section"
+        >
+          <motion.div
+            animate={{ y: [0, 5, 0] }}
+            transition={{ repeat: Infinity, duration: 1.5, ease: "easeInOut" }}
+          >
+            <ChevronUp className="w-6 h-6 md:w-8 md:h-8 group-hover:text-[#D4A03E] transition-colors duration-300" />
+          </motion.div>
+        </motion.button>
+      )}
+    </>
+  );
+};
+
+export default Socials;
